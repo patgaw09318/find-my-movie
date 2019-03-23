e1d6e904a02778276678a010d23b73fe9ed2b61c
Patryk Gawryszewski <patgaw09318@gmail.com>
2019-03-23 20:14:36 +0100

    Cleanup of create-react-app template

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header" />
+        <body className="App-body" />
+        <footer className="App-footer" />
+      </div>
+    );
+  }
+}
+
+export default App;
