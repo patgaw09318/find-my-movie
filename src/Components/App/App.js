36a7f0f1fb02da4a1cb06aeca8505e9c0cdeb5ef
Patryk Gawryszewski <patgaw09318@gmail.com>
2019-03-24 01:47:52 +0100

    Search component - Asynchronous Searching

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,16 +3,12 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Search from "../Search/Search";
 import NavBar from "../NavBar/NavBar";
-
 class App extends Component {
   render() {
-    const options = ["Test1", "Test2", "Test3"];
     return (
       <div className="App">
         <NavBar />
-        <body className="App-body">
-          <Search options={options} />
-        </body>
+        <Search />
       </div>
     );
   }
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,21 +1,60 @@
 import React, { Component } from "react";
-import { Typeahead } from "react-bootstrap-typeahead";
 import "react-bootstrap-typeahead/css/Typeahead.css";
+import { asyncContainer, Typeahead } from "react-bootstrap-typeahead";
+import axios from "axios";
+const AsyncTypeahead = asyncContainer(Typeahead);
+
+const apiKey = "cfe422613b250f702980a3bbf9e90716";
 
 class Search extends Component {
   state = {
-    multiple: false
+    allowNew: false,
+    isLoading: false,
+    multiple: false,
+    options: []
   };
 
+  _handleSearch = query => {
+    this.setState({ isLoading: true });
+    this.makeAndHandleRequest(query);
+  };
+
+  makeAndHandleRequest = query => {
+    axios
+      .get("https://api.themoviedb.org/3/search/movie", {
+        params: {
+          api_key: apiKey,
+          include_adult: true,
+          page: 1,
+          language: "en-US",
+          query: query
+        }
+      })
+      .then(response => {
+        const options = response.data.results.map(item => item["title"]);
+        console.log(options);
+        this.setState({
+          isLoading: false,
+          options
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
   render() {
     return (
-      <form>
-        <Typeahead
-          labelKey="name"
-          options={this.props.options}
+      <div>
+        <AsyncTypeahead
+          isLoading={this.state.isLoading}
+          id="search"
+          minLength={3}
           placeholder="Choose a state..."
+          onSearch={this._handleSearch}
+          options={this.state.options}
+          onChange={value => console.log("selected: " + value)}
         />
-      </form>
+      </div>
     );
   }
 }
