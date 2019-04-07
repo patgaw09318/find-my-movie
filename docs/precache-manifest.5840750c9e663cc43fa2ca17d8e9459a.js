d84160897c2395f97cb2018b4b3ba5315f43db43
Patryk Gawryszewski <patgaw09318@gmail.com>
2019-04-07 11:38:56 +0200

    Demo package added

---
+++ docs/precache-manifest.5840750c9e663cc43fa2ca17d8e9459a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2edd399eba14dee0da93",
+    "url": "/static/css/main.fb2d3f20.chunk.css"
+  },
+  {
+    "revision": "2edd399eba14dee0da93",
+    "url": "/static/js/main.0f86e027.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7d294a65622aaa8431d5",
+    "url": "/static/css/2.06330f7e.chunk.css"
+  },
+  {
+    "revision": "7d294a65622aaa8431d5",
+    "url": "/static/js/2.93e1bdf9.chunk.js"
+  },
+  {
+    "revision": "a9528ec2c7d8ab3d350e1cbff4992fb8",
+    "url": "/index.html"
+  }
+];
